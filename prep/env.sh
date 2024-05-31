2e7325399bea08083cb0fb25e7e5f53121768f30
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2024-05-31 15:20:57 +0000

    Persistant ssh storage on ~/work

--- prep/env.sh
+++ prep/env.sh
@@ -8,6 +8,8 @@ fi
 
 export SSH_AUTH_SOCK=/run/lock/agent.$USER
 if [ ! -S $SSH_AUTH_SOCK ] ; then
+    install -dpv -m 700 ~/work/.ssh
+    ln -sfv ~/work/.ssh ~/
     eval `ssh-agent -a $SSH_AUTH_SOCK` | grep -v "^Agent pid"
     ssh-add ~/work/.ssh/id_rsa
 fi
